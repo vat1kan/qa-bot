107f25aab75ecc1d45508c7fe99f013fe9dd0bae
vat1kan <gariksgs044@gmail.com>
2024-01-08 18:55:01 +0200

    restructed as dialog format

---
+++ Formating.py
@@ -0,0 +1,65 @@
+from Dictionary import dict
+
+def Split(message, separator):
+    if message[-1] == '.':
+        message = message[:-1:]
+        message = message.split(separator)
+    else: message = message.split(separator)
+    return message
+
+def DeleteSpace(message):
+    if type(message) == str:
+        while message[0] == " ":
+            message = message[1::]
+        while message[-1] == " ":
+            message = message[:-1:]
+    elif type(message) == list:
+        for index in range(len(message)):
+            while message[index][0] == " ":
+                message[index] = message[index][1::]
+            while message[index][-1] == " ":
+                message[index] = message[index][:-1:]
+    return message
+
+def Capitalize(message):
+    message = DeleteSpace(message)
+    if type(message) == str:
+        if message[0].islower() == True:
+            message = message.capitalize()
+    elif type(message) == list:
+        for index in range(len(message)):
+            if message[index][0].islower() == True:
+                message[index] = message[index].capitalize()
+    return message
+
+def Point(message):
+    message = Capitalize(message)
+    if type(message) == str:
+        if message[-1] != ".":
+            message = "{0}. ".format(message)
+    elif type(message) == list:
+        for index in range(len(message)):
+            if message[index][-1] != ".":
+                message[index] = "{0}. ".format(message[index])
+    return message
+
+def DescriptFormating(message) -> list:
+    message = Point(Split(message,'.'))
+    return message
+
+def StepsFormating(message) -> list:
+    message = Point(Split(message,'.'))
+    for index in range(len(message)):
+        message[index] = f"{index+1}) " + message[index] + "\n"
+    return message
+
+def PlatformFormating(message) -> list:
+    message = DeleteSpace(Split(message, '.'))
+    platform = {v: k for k, values in dict.items() for v in values}
+    for index in range(len(message)):
+        message[index] = "\n" + platform.get(message[index], message[index]) + "."
+    return message
+
+def about_text() -> str:
+    message = "This bot is designed to create bug reports while testing.\n\nThe proposed structure consists of media file (image or video), title, description, reproduction steps, severity and bug detection environment.\n\nType the command '<b>/start</b>' to start the creation. You can skip the bug report field by typing the word '<code>pass</code>' in the message.\n\nTo cancel the creation, enter the command '<b>/cancel</b>'. Canceling will delete some messages in chat with the bot.\n\nContact the owner for more information: @vat1kan."
+    return message
---
+++ main.py
@@ -0,0 +1,163 @@
+import asyncio
+import logging
+import time
+import os
+import sys
+from typing import Any, Dict
+from aiogram import Bot, Dispatcher, F, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from Formating import Point, DescriptFormating, StepsFormating, PlatformFormating, about_text
+
+TOKEN = os.getenv('TOKEN')
+
+br_router = Router()
+ids = []
+
+class Report(StatesGroup):
+    media_file = State()
+    title = State()
+    description = State()
+    steps = State()
+    severity = State()
+    environment = State()
+
+@br_router.message(CommandStart())
+async def command_start(message: Message, state: FSMContext) -> None:
+    await state.set_state(Report.media_file)
+    msg = await message.answer("Reporting started. Upload your media file:")
+    global chat_id
+    chat_id = message.chat.id
+    ids.extend((message.message_id, msg.message_id))
+
+@br_router.message(Command("cancel"))
+@br_router.message(F.text.casefold() == "/cancel")
+async def cancel_handler(message: Message, state: FSMContext) -> None:
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    logging.info("Cancelling state %r", current_state)
+    ids.append(message.message_id)
+    await state.clear()
+    for id in ids: 
+        await bot.delete_message(chat_id, id)
+    ids.clear()
+    cancel = await message.answer("Reporting cancelled.")
+    time.sleep(3)
+    await bot.delete_message(chat_id, cancel.message_id)
+
+@br_router.message(Command("info"))
+@br_router.message(F.text.casefold() == "/info")
+async def info(message: Message)-> None:
+    msg = await message.answer(f"{about_text()}")
+    ids.extend((message.message_id, msg.message_id))
+
+@br_router.message(Report.media_file)
+async def process_media(message: Message, state: FSMContext) -> None:
+    await state.update_data(media_file=message)
+    await state.set_state(Report.title)
+    msg = await message.answer("Enter bug report title.")
+    ids.extend((message.message_id, msg.message_id))
+
+@br_router.message(Report.title)
+async def process_title (message: Message, state: FSMContext) -> None:
+    await state.update_data(title=message.text)
+    await state.set_state(Report.description)
+    msg = await message.answer("Enter bug description.")
+    ids.extend((message.message_id, msg.message_id))
+
+@br_router.message(Report.description)
+async def process_description (message: Message, state: FSMContext) -> None:
+    await state.update_data(description=message.text)
+    await state.set_state(Report.steps)
+    msg = await message.answer("Enter steps to bug reproduce.")
+    ids.extend((message.message_id, msg.message_id))
+
+@br_router.message(Report.steps)
+async def process_steps (message: Message, state: FSMContext) -> None:
+    await state.update_data(steps=message.text)
+    await state.set_state(Report.severity)
+    msg = await message.answer("Enter the severity of the bug.")
+    ids.extend((message.message_id, msg.message_id))
+
+@br_router.message(Report.severity)
+async def process_severity (message: Message, state: FSMContext) -> None:
+    await state.update_data(severity=message.text)
+    await state.set_state(Report.environment)
+    msg = await message.answer("Specify the environment in which the bug is detected.")
+    ids.extend((message.message_id, msg.message_id))
+
+@br_router.message(Report.environment)
+async def process_environment (message: Message, state: FSMContext) -> None:
+    data = await state.update_data(environment=message.text)
+    await show_summary(message=message, data=data)
+    ids.append(message.message_id)
+    await state.clear()
+
+@br_router.message(Report.environment)
+async def show_summary(message: Message, data: Dict[str, Any]) -> None:
+    try:
+        builder = InlineKeyboardBuilder()
+        builder.add(types.InlineKeyboardButton(text='Delete', callback_data='clear'))
+        
+        media_file = data.get("media_file")
+        br_text = ""
+
+        title = data.get('title')
+        if title != 'pass':
+            title = Point(title)
+            br_text += f"<b>Title:</b> {''.join(title)}\n\n"
+        
+        description = data.get('description')
+        if description != 'pass':
+            description = DescriptFormating(description)
+            br_text += f"<b>Description:</b>\n{''.join(description)}\n\n"
+        
+        steps = data.get('steps')
+        if steps != 'pass':
+            steps = StepsFormating(steps)
+            br_text += f"<b>Steps:</b>\n{''.join(steps)}\n"
+        
+        severity = data.get('severity')
+        if severity != 'pass':
+            severity = Point(severity)
+            br_text+= f"<b>Severity:</b>\n{''.join(severity)}\n\n"
+        
+        environment = data.get('environment')
+        if environment != 'pass':
+            environment = PlatformFormating(environment)
+            br_text += f"<b>Environment:</b>{''.join(environment)}"
+        
+        if media_file.photo:
+            msg = await bot.send_photo(message.chat.id, media_file.photo[-1].file_id, caption=f"{br_text}", reply_markup=builder.as_markup())
+            ids.append(msg.message_id)
+        elif media_file.video:
+            msg = await bot.send_video(message.chat.id, media_file.video.file_id, caption=f"{br_text}", reply_markup=builder.as_markup())
+            ids.append(msg.message_id)
+        else:
+            msg = await message.answer(f"{br_text}", reply_markup=builder.as_markup())
+            ids.append(msg.message_id)
+    except Exception as e:
+        await message.answer(text=f"<b>Some error occurred. Debug message:</b>\n\n<code>{e}</code>", parse_mode='html')
+
+@br_router.callback_query()
+async def clear(callback_query: types.CallbackQuery) -> Any:
+    for id in ids: 
+        await bot.delete_message(chat_id, id)
+    ids.clear()
+
+async def main():
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(br_router)
+    await dp.start_polling(bot)
+
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+asyncio.run(main())
